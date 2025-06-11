6c9886fe0542f2dd4d43e896fe61aeccc75728d9
Mosleh92 <121885983+Mosleh92@users.noreply.github.com>
2025-06-11 10:54:00 +0400

    Create page.tsx

---
+++ mall-permit-system/app/memos/page.tsx
@@ -0,0 +1,9 @@
+import MemoBoard from '@/components/MemoBoard';
+
+export default function MemoPage() {
+  return (
+    <div className="max-w-2xl mx-auto mt-10">
+      <MemoBoard />
+    </div>
+  );
+}
